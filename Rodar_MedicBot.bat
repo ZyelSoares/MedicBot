18fda0b3cad60a5ba71bd4b9dcba3e6dba485a81
ZyelSoares <josielfacc@gmail.com>
2026-02-02 10:36:36 -0300

    Remove arquivos opcionais, deixa só o necessário

--- Rodar_MedicBot.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-REM Inicia o MedicBot e fecha o CMD na hora (evita janela atrás).
-REM Prefira usar Rodar_MedicBot.vbs para não ver nenhum terminal.
-start /B pythonw medicbot.py
-exit
